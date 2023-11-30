922a910716013621cd144af96eab4e7cfc46933c
rachele <rachelecerasoli6@gmail.com>
2023-11-30 17:48:43 +0100

    modifiche

--- exercise.js
+++ exercise.js
@@ -1,3 +1,11 @@
-let hello = 'Ciao';     // boolean
-let age = 18;       // string
-let isGraduated = false;     // number
+let hello = 'Ciao';
+let age = 18;
+let isGraduated = false;
+
+let helloBoolean = hello == 'Ciao';
+let agestr = String(age);
+let isGraduatedInt = Number(isGraduated);
+
+console.log(typeof (helloBoolean), helloBoolean);
+console.log(typeof (agestr), agestr);
+console.log(typeof (isGraduatedInt), isGraduatedInt);
